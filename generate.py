e53c707d691999e26559f2a9dd3780276b8c2a7f
Patrick Galliani <pjgalliani@caltech.edu>
2017-02-23 10:13:18 -0800

    Deleted unnecessary files, added HMM module

---
+++ generate.py
@@ -0,0 +1,11 @@
+# Ricky Galliani, David Kawashima, Eshan Govil
+# Februray 2017
+
+out_quatrains= 'out_quatrains.csv' 
+out_voltas = 'out_voltas.csv' 
+out_couplets = 'out_couplets.csv'
+out_rhymes = 'out_rhymes.json'
+out_quatrain_w_to_i = 'out_q_w_map.json' 
+out_volta_w_to_i = 'out_v_w_map.json' 
+out_couplets_w_to_i = 'out_c_w_map.json'  
+
